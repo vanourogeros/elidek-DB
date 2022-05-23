6207386c6997cc6a7185591fb9ba54801c8a1c0b
vanourogeros <61976410+vanourogeros@users.noreply.github.com>
2022-05-23 23:56:28 +0300

    Added /projects
    with filtering capability

---
+++ elidekdb/forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, IntegerField, DateField, IntegerRangeField
+from wtforms.validators import DataRequired, Email, Optional, NumberRange
+
+## when passed as a parameter to a template, an object of this class will be rendered as a regular HTML form
+## with the additional restrictions specified for each field
+class ProjectFilterForm(FlaskForm):
+    min_Start_Date = DateField(label = "Start Date From:", validators = [Optional()])
+
+    max_Start_Date = DateField(label = "Start Date To:", validators = [Optional()])
+
+    min_End_Date = DateField(label = "End Date From:", validators = [Optional()])
+
+    max_End_Date = DateField(label = "End Date To:", validators = [Optional()])
+
+    min_Duration = IntegerField(label = "Duration From (in days)", validators = [Optional(), NumberRange(min=365, max=1460)])
+
+    max_Duration = IntegerField(label = "Duration To (in days)", validators = [Optional(), NumberRange(min=365, max=1460)])
+
+    executive = StringField(label = "Executive Name", validators = [Optional()])
+
+    submit = SubmitField("Filter")
--- elidekdb/routes.py
+++ elidekdb/routes.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, flash, redirect, url_for, abort
 from flask_mysqldb import MySQL
 from elidekdb import app, db ## initially created by __init__.py, need to be used here
+from elidekdb.forms import *
 
 @app.route("/")
 def index():
@@ -23,3 +24,58 @@ def programs_view():
     #print(programs[1])
 
     return render_template("programs.html", programs=programs, pageTitle = "Programs Page")
+
+@app.route("/projects", methods = ['GET', 'POST'])
+def projects_view():
+    form = ProjectFilterForm()
+    cur = db.connection.cursor()   
+
+    query = """
+    SELECT Project_ID, P.Name AS P_Name, Summary, Project_Funds, Start_Date, End_Date, CONCAT(E.Name, ' ',  E.Surname) AS E_Name, Organization_ID
+    FROM Project P INNER JOIN Executive E 
+    ON P.Executive_ID = E.Executive_ID
+    """
+
+    if(request.method == "POST" and form.validate_on_submit()):
+        min_Start_Date = str(request.form.get('min_Start_Date'))
+        max_Start_Date = str(request.form.get('max_Start_Date'))
+        min_End_Date = str(request.form.get('min_End_Date'))
+        max_End_Date = str(request.form.get('max_End_Date'))
+        min_Duration = str(request.form.get('min_Duration'))
+        max_Duration = str(request.form.get('max_Duration'))
+        executive = str(request.form.get('executive'))
+        print("form!")
+        print(max_Start_Date)
+        where_or_and = 'WHERE'
+        if min_Start_Date != '':
+            query += f'WHERE DATEDIFF(P.Start_Date, \'{min_Start_Date}\') > 0'
+            where_or_and = '\n    AND'
+        if max_Start_Date != '':
+            query += f'{where_or_and} DATEDIFF(P.Start_Date, \'{max_Start_Date}\') < 0'
+            where_or_and = '\n    AND'
+        if min_End_Date != '':
+            query += f'{where_or_and} DATEDIFF(P.End_Date, \'{min_End_Date}\') > 0'
+            where_or_and = '\n    AND'
+        if max_End_Date != '':
+            query += f'{where_or_and} DATEDIFF(P.End_Date, \'{max_End_Date}\') < 0'
+            where_or_and = '\n    AND'
+        if min_Duration != '':
+            query += f'{where_or_and} DATEDIFF(P.End_Date, P.Start_Date) > {min_Duration}'
+            where_or_and = '\n    AND'
+        if max_Duration != '':
+            query += f'{where_or_and} DATEDIFF(P.End_Date, P.Start_Date) < {max_Duration}'
+            where_or_and = '\n    AND'
+        if executive != '':
+            query += f'{where_or_and} CONCAT(E.Name, \' \',  E.Surname) = \'{executive}\''
+            where_or_and = '\n    AND'
+
+    print(query)
+    cur.execute(query)
+    column_names = [i[0] for i in cur.description]
+    projects = [dict(zip(column_names, entry)) for entry in cur.fetchall()]
+    #print(projects)
+    #programs = cur.fetchall()
+    cur.close()
+    #print(programs[1])
+
+    return render_template("projects.html", projects=projects, pageTitle = "Projects Page", form = form)
